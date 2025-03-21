abd5839aac4a64752489b83c01f41e5feb4809d8
kawanmoura <https://github.com/kawanmoura>
2025-03-21 14:51:02 -0300

    sport elevate

--- 19-03/script.js
+++ 19-03/script.js
@@ -1,1 +1,17 @@
-alert("olá");
+const email = document.querySelector("#email");
+const senha = document.querySelector("#senha");
+const button = document.querySelector("#button");
+const successMessage = document.querySelector("#success-message");
+
+function cadUser(event) {
+    event.preventDefault();
+    const emailValue = email.value;
+    const senhaValue = senha.value;
+
+    if (emailValue && senhaValue) {
+        successMessage.style.display = "block";
+        email.value = "";
+        senha.value = "";
+    }
+}
+button.addEventListener("click", cadUser);
